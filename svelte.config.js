6f017eab80e88fe3c90b5a75164ab74aed858e68
paoloricciuti <ricciutipaolo@gmail.com>
2025-11-22 01:23:34 +0100

    fix: trust origins

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter(), experimental: { remoteFunctions: true } },
+	kit: {
+		adapter: adapter(),
+		experimental: { remoteFunctions: true },
+		csrf: { trustedOrigins: ['*'] }
+	},
 	compilerOptions: {
 		experimental: {
 			async: true
